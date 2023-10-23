94fef49684e3ac8f4eaa896142498782614c4c49
spidermatsu <62445737+spidermatsu@users.noreply.github.com>
2023-10-23 18:18:46 +0100

    Add files via upload

---
+++ week 2 - 2.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      fuseb
+#
+# Created:     09/10/2023
+# Copyright:   (c) fuseb 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import string
+string.ascii_lowercase
+'abcdefghijklmnopqrstuvwxyz'
+alphabetlist = list(string.ascii_lowercase)
+i = 0
+#while True:
+ #   try:
+  #       num1 = string(input("Enter a sentence"))
+
+for i in range (1, len(alphabetlist)):
+    print(i)
+    i ++z
